7f1d122e516df9248ce0d4ef867d508a08973bbe
tincho4t <martinforte@yahoo.com.ar>
2016-10-31 10:15:13 -0300

    KOHONEM II

--- TP2/mainK.py
+++ TP2/mainK.py
@@ -6,83 +6,31 @@ from NN_KOHONEM import NN_KOHONEM
 import numpy as np
 import matplotlib.pyplot as plt
 from numpy import genfromtxt
-from random import randint
 import cPickle
 import argparse
-import sklearn.metrics as metrics
+import math
+# from random import randint
 
-# parser = argparse.ArgumentParser(description='TP 1 de Redes Neuronales')
-# parser.add_argument('--problem', metavar='problem', type=int, help='Problema 1 o 2. Valores esperados: 1 o 2')
-# parser.add_argument('--mode', metavar='mode', type=str, help='Modo en que se utilizara: "training"/"test"')
-# parser.add_argument('--input', type=str, help='File .csv con la informacion que sera utilizada')
+parser = argparse.ArgumentParser(description='TP 2 de Redes Neuronales Kohonem')
+parser.add_argument('--mode', metavar='mode', type=str, help='Modo en que se utilizara: "training"/"test"')
+parser.add_argument('--input', type=str, help='File .csv con la informacion que sera utilizada')
 
-# # Parametros solo necesarios para el modo: test
-# parser.add_argument('--load', type=str, help='Ruta del dump de la red.')
+# Parametros solo necesarios para el modo: test
+parser.add_argument('--load', type=str, help='Ruta del dump de la red.')
 
-# # Parametros solo necesarios para el modo: train
-# parser.add_argument('--save', type=str, help='Ruta donde se guardara la red entrenada.')
-# parser.add_argument('--layers', metavar='layers', type=int, nargs='+', help='Solo requerido en modo: training. Lista de int con el tamanio de las capas intermedias de la red. Ej: "--layers 2 3" resulta en la red [_,2,3,1] para el problema 1.') # Esto se hace asi ya que al discretizar las variables generamos un numero mayor y no podemos permitir que se parametricen la entrada ni tampoco tiene sentido la salida.
-# parser.add_argument('--lr', metavar='lr', type=float, help='Learning Rate')
-# parser.add_argument('--epocs', type=int, help='Cantidad de epocs de entrenamiento.')
-# parser.add_argument('--testProportion', type=float, help='Aplica a Problema 2 unicamente. Si se setea este parametro entonces se divide el train set en dos partes y se mide el Error Cuadratico Medio')
+# Parametros solo necesarios para el modo: train
+parser.add_argument('--save', type=str, help='Ruta donde se guardara la red entrenada.')
+parser.add_argument('--lr', metavar='lr', type=float, help='Learning Rate')
+parser.add_argument('--s0', metavar='s0', type=float, help='Sigma inicial')
+parser.add_argument('--sr', metavar='sr', type=float, help='Sigma r')
+parser.add_argument('--epocs', type=int, help='Cantidad de epocs de entrenamiento.')
+parser.add_argument('--m1', type=int, help='Cantidad de filas de la matriz de salida.')
+parser.add_argument('--m2', type=int, help='Cantidad de columnas de la matriz de salida.')
+parser.add_argument('--testProportion', type=float, help='Aplica a Problema 2 unicamente. Si se setea este parametro entonces se divide el train set en dos partes y se mide el Error Cuadratico Medio')
 
-# args = parser.parse_args()
+args = parser.parse_args()
 
 # print args
-
-###############################################################################
-###################### DATASETS TRIVIALES DE PRUEBA ###########################
-###############################################################################
-
-def andDataset():
-	X = np.array([[0,0],[0,1],[1,0],[1,1]])
-	Z = np.array([0,0,0,1])
-	return (X,Z)
-
-def orDataset():
-	X = np.array([[0,0],[0,1],[1,0],[1,1]])
-	Z = np.array([0,1,1,1])
-	return (X,Z)
-
-def xorDataset():
-	X = np.array([[0,0],[0,1],[1,0],[1,1]])
-	Z = np.array([0,1,1,0])
-	layers = list([2,3,1])
-	return (X,Z,layers)
-
-
-###############################################################################
-########################## CALCULAR PERFORMANCE ###############################
-###############################################################################
-
-def auc(positivePredictions, negativePredictions):
-	repeateTimes = 100 * max(len(positivePredictions),len(negativePredictions))
-	acum = 0.0
-	for i in range(repeateTimes):
-		positiveIndex = randint(0,len(positivePredictions) -1)
-		negativeIndex = randint(0,len(negativePredictions) -1)
-		acum += positivePredictions[positiveIndex] > negativePredictions[negativeIndex]
-	return(acum/repeateTimes)
-
-def calcAuc(Z, Zhat):
-	positivePredictions = Zhat[Z[0:100]==1]
-	negativePredictions = Zhat[Z[0:100]==0]
-	print("Roc area %f " % auc(positivePredictions,negativePredictions))
-
-def rmse(Z, Zhat):
-	error = Z - Zhat
-	error*=error
-	error = np.mean(error)
-	error = np.power(error,1.0/2.0)
-	return(error)
-
-def calcRmseEj2(Z, Zhat):
-	print("First Column RMSE: ",rmse(Z[:,0], Zhat[:,0, 0]))
-	print("Second Column RMSE: ",rmse(Z[:,1], Zhat[:,0, 1]))
-
-def compareResults(z, zhat):
-	for j in range(10):
-		print("%f - %f ====== %f - %f" %(z[j,0], zhat[j,0], z[j,1], zhat[j,1]))
 
 ###############################################################################
 ############################ DATASET ADMINISTRATOR ############################
@@ -98,16 +46,6 @@ def splitSet(X, Z, testProportion = 0.2):
 	X_test = X[trainIndex[n_train:], :]
 	Z_test = Z[trainIndex[n_train:]]
 	return(X_train,Z_train,X_test,Z_test)
-
-# Normaliza vectores y los centra entre 0 y 1. Devuelve los vectorres normalizados y los mínimos y máximos
-def normalize(Z):
-	minTrain = Z.min()
-	maxTrain = Z.max()
-	return ((Z - minTrain)/(maxTrain-minTrain), minTrain, maxTrain)
-
-# Desnormaliza el vector para volverlo a los rangos iniciales
-def denormalize(Z, minTrain, maxTrain):
-	return Z*(maxTrain-minTrain)+minTrain
 
 ###############################################################################
 ################################ DUMPING ######################################
@@ -135,15 +73,6 @@ def getTrainingParamsEj1(filenameInput):
 	Z, X = getDataset(filenameInput)
 	return (X,Z,X.shape[1])
 
-def getTrainingParamsEj2(inputLayers, filenameInput, saveIn):
-	saveIn = saveIn if saveIn else './' # Si no viene definodo el parametro tomo ./ como default
-	# dn = DatasetNormalizer(filenameInput, 'ej2')
-	X = dn.discretization(dn.data[:,0:8], dn.EJ2_COLUMNS_NAME, save_path=saveIn+'-normalizer.pkl')
-	Z = dn.data[:,8:]
-	layers = [X.shape[1]] + inputLayers + [2]
-	activationFunctions = getSigmoidFunctions(len(layers))
-	return (X,Z,layers,activationFunctions)
-
 def showMatrix(xPositions, matrixDimension):
 	M = np.zeros(matrixDimension)
 	for i in xPositions:
@@ -152,7 +81,7 @@ def showMatrix(xPositions, matrixDimension):
 	# plt.imshow(M/np.max(M), cmap='hot', interpolation='nearest')
 	# plt.show()
 
-def trainEj1(matrixDimension, filenameInput, epocs, lr, s0, sr, saveIn=None):
+def train(matrixDimension, filenameInput, epocs, lr, s0, sr, saveIn=None):
 	X, Z, inputShape = getTrainingParamsEj1(filenameInput)
 	
 	nn = NN_KOHONEM(inputShape, matrixDimension, lr, s0, sr)
@@ -167,79 +96,19 @@ def trainEj1(matrixDimension, filenameInput, epocs, lr, s0, sr, saveIn=None):
 	return nn
 
 
-def trainEj2(inputLayers, lr, filenameInput, epocs, saveIn=None, testProportion=None):
-	X, Z, layers, activationFunctions = getTrainingParamsEj2(inputLayers, filenameInput, saveIn)
-	tp = testProportion if testProportion else 0.0
-	X_train, Z_train, X_test, Z_test = splitSet(X, Z, testProportion=tp)
-	Z_train, minTrain, maxTrain = normalize(Z_train)
-	nn = NN(layers, activationFunctions, lr)
-	##
-	acum = 0
-	interval = 20
-	for i in range(epocs):
-		e = nn.mini_batch(X_train,Z_train)
-		print "Epoc %d error: %f" % (i, e)
-		if(testProportion and i%interval == 0):
-			print("Train RMSE")
-			Zhat = nn.predict(X_train)
-			calcRmseEj2(Z_train, Zhat)
-			print("Test RMSE")
-			Zhat = nn.predict(X_test)
-			Zhat = denormalize(Zhat, minTrain, maxTrain)
-			calcRmseEj2(Z_test, Zhat)
-	if (saveIn):
-		saveAs(saveIn, (nn, minTrain, maxTrain))
-
-# Lo usamos para probar distinas configuraciones
-def experimentWithEj2():
-	# lr = 0.0001
-	# neurons = 1000
-	for lr in [0.01]:
-		for neurons in [10,50,100,500,2000]:
-			print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-			print "lr: %f, neurons: %d" % (lr, neurons)
-			print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>"
-			trainEj2([neurons], lr, 2000, './data/tp1_ej2_training.csv')
-
 ###############################################################################
 ################################## TEST #######################################
 ###############################################################################
 
-def testEj1(loadFrom, filenameInput):
+def test(loadFrom, filenameInput):
 	nn = load(loadFrom)
-	dn = DatasetNormalizer(filenameInput, 'ej1', test=True)
-	X  = dn.apply_discretization(dn.data, dn.EJ1_COLUMNS_NAME, loadFrom+'-normalizer.pkl')
+	X  = genfromtxt(filename, delimiter=',')
 	Zhat = nn.predict(X)
 	print "Predictions:"
 	for z in Zhat:
 		print "%f" % z[0][0]
 
-def testEj2(loadFrom, filenameInput):
-	nn, minTrain, maxTrain = load(loadFrom)
-	dn = DatasetNormalizer(filenameInput, 'ej2', test=True)
-	X  = dn.apply_discretization(dn.data, dn.EJ2_COLUMNS_NAME, loadFrom+'-normalizer.pkl')
-	Zhat = nn.predict(X)
-	Zhat = denormalize(Zhat, minTrain, maxTrain)
-	print "Predictions:"
-	for z in Zhat:
-		print "%f, %f" % (z[0][0],z[0][1])
-
-
-# if (args.mode == 'training'):
-# 	if (args.problem == 1):
-# 		trainEj1(args.layers, args.lr , args.input, args.epocs, saveIn=args.save)
-# 	elif (args.problem == 2):
-# 		trainEj2(args.layers, args.lr , args.input, args.epocs, saveIn=args.save, testProportion=args.testProportion)
-# elif (args.mode == 'test'):
-# 	if (args.problem == 1):
-# 		testEj1(args.load, args.input)
-# 	elif (args.problem == 2):
-# 		testEj2(args.load, args.input)
-
-# trainEj1([200], 0.01, 5000, './data/tp1_ej1_training.csv')
-# experimentWithEj2()
-
-def getMatrixForClass(matrixDimension, xPositions, category):
+def getMatrixForClass(matrixDimension, xPositions, category, Z):
 	M = np.zeros(matrixDimension)
 	for i in range(len(xPositions)):
 		p = xPositions[i]
@@ -250,27 +119,23 @@ def getMatrixForClass(matrixDimension, xPositions, category):
 def heatMap(nn, filename, matrixDimension, category = None):
 	X, Z, _ = getTrainingParamsEj1(filename)
 	xPositions = nn.predict(X)
-	M = getMatrixForClass(matrixDimension, xPositions, category)
+	M = getMatrixForClass(matrixDimension, xPositions, category, Z)
 	print M
 	plt.imshow(M/np.max(M), cmap='hot', interpolation='nearest')
 	plt.show()
 
-def createEvaluateVari2ables(matrixDimension, filenameInput, lr, s0, sr, saveIn=None):
+def createEvaluateVariables(matrixDimension, filenameInput, lr, s0, sr, tp=0.2):
 	X, Z, inputShape = getTrainingParamsEj1(filenameInput)
 	nn = NN_KOHONEM(inputShape, matrixDimension, lr, s0, sr)
-	return nn,X,Z
+	X_train, Z_train, X_test, Z_test = splitSet(X, Z, testProportion=tp)
+	return nn,X_train, Z_train, X_test, Z_test
 
-# def evaluate(nn, X,Z):
-# 	xPositions = nn.predict(X)
-# 	return metrics.silhouette_score(xPositions, Z)
-
-def trainAndEval(nn, X, Z, epocs, initialEpoc = 0):
+def trainAndEval(nn, X_train, Z_train, X_test, Z_test, epocs, initialEpoc = 0):
 	for i in range(epocs):
-		nn.mini_batch(X)
-	return evaluate(nn, X,Z)
+		nn.mini_batch(X_train)
+	return evaluate(nn, X_train, Z_train, X_test, Z_test)
 
 ############ Metrica Distancia a Centroides ############
-import math
 
 # Devuelve el indice del mayor indice
 def getMaxIndex(M):
@@ -281,14 +146,14 @@ def getMaxIndex(M):
 				maxIndex = (i,j)
 	return maxIndex
 
-def calculateCentroid(xPositions, z, matrixDimension):
-	M = getMatrixForClass(matrixDimension, xPositions, z)
+def calculateCentroid(xPositions, z, matrixDimension, Z):
+	M = getMatrixForClass(matrixDimension, xPositions, z, Z)
 	return getMaxIndex(M)
 
 def calculateCentroids(xPositions, Z, matrixDimension):
 	centroids = {}
 	for z in Z:
-		centroids[z] = calculateCentroid(xPositions, z, matrixDimension)
+		centroids[z] = calculateCentroid(xPositions, z, matrixDimension, Z)
 	return centroids
 
 def distanceBetween(x,y):
@@ -322,61 +187,60 @@ def getDistancesOfCentroids(xPositions, centroids, Z):
 def sumInstancesOf(Z,z):
 	return np.sum(Z == z)
 
-# Mira la diferencia de distancia entre las intancias
-def getError(nn, xPositions, Z, centroids):
-	distances = getDistancesOfCentroids(xPositions, centroids, Z)
-	errors = []
-	for z in np.unique(Z):
-		okClasificated = len(distances[z][0])
-		total = sumInstancesOf(Z,z)
-		print "Centroide %d -> Total %f --- ok: %d total: %d Diff: %f" % (z, (okClasificated/total) - (np.sum(distances[z][0])/ np.sum(distances[z][1])), okClasificated, total, (np.sum(distances[z][0])/ np.sum(distances[z][1])))
-		error = (okClasificated/total) - (np.sum(distances[z][0])/ np.sum(distances[z][1]))
-		errors.append(error)
-	return errors
+def getAcu(distances, z, Z):
+	okClasificated = len(distances[z][0])
+	total = sumInstancesOf(Z,z)
+	return okClasificated/total
 
-def getScore(nn, xPositions, Z, centroids):
-	distances = getDistancesOfCentroids(xPositions, centroids, Z)
-	scores = []
-	acu = []
+def getDistanceToNearestCentroid(nn, centroids, z):
+	#Lo hago asi porque ya tengo codeadas las funciones
+	_, nearestCentroid = getNearestCentroids(centroids[z],centroids) # Como el 1ro voy a ser yo mismo tomo el 2do
+	return distanceBetween(centroids[z], nearestCentroid) / math.hypot(nn.M1,nn.M2)
+
+def getScore(nn, x_trainPositions, Z_train, x_testPositions, Z_test, centroids):
+	distances_train = getDistancesOfCentroids(x_trainPositions, centroids, Z_train)
+	distances_test  = getDistancesOfCentroids(x_testPositions, centroids, Z_test)
+	acus_train = []
+	acus_test = []
 	d = []
-	for z in np.unique(Z):
-		okClasificated = len(distances[z][0])
-		total = sumInstancesOf(Z,z)
-		##########
-		#Lo hago asi porque ya tengo codeadas las funciones
-		_, nearestCentroid = getNearestCentroids(centroids[z],centroids) # Como el 1ro voy a ser yo mismo tomo el 2do
-		print centroids[z], nearestCentroid, distanceBetween(centroids[z], nearestCentroid)
-		distanceToNearestCentroid = distanceBetween(centroids[z], nearestCentroid) / math.hypot(nn.M1,nn.M2)
-		#########
-		score = (okClasificated/total) + distanceToNearestCentroid
+	for z in np.unique(Z_train):
+		acu_train = getAcu(distances_train, z, Z_train)
+		acu_test  = getAcu(distances_test, z, Z_test)
+		distanceToNearestCentroid = getDistanceToNearestCentroid(nn, centroids, z)
 		# print "Centroide %d -> Total %f --- Acu: %f(%d/%d) Distance: %f" % (z, score, okClasificated/total,okClasificated, total, distanceToNearestCentroid)
-		scores.append(score)
-		acu.append(okClasificated/total)
+		acus_train.append(acu_train)
+		acus_test.append(acu_test)
 		d.append(distanceToNearestCentroid)
-	zAmount = len(np.unique(Z))
-	return (np.average(scores), np.average(acu), np.average(d))
+	return (np.average(acus_train), np.average(acus_test), np.average(d))
 
-def evaluate(nn, X, Z):
-	xPositions = nn.predict(X)
-	centroids = calculateCentroids(xPositions, Z, (nn.M1,nn.M2))
-	errors = getError(nn, xPositions, Z, centroids)
-	# return np.sum(errors)
-	return getScore(nn, xPositions, Z, centroids), np.sum(errors)
+def evaluate(nn, X_train, Z_train, X_test, Z_test):
+	x_trainPositions = nn.predict(X_train)
+	x_testPositions = nn.predict(X_test)
+	centroids = calculateCentroids(x_trainPositions, Z_train, (nn.M1,nn.M2))
+	return getScore(nn, x_trainPositions, Z_train, x_testPositions, Z_test, centroids)
 
 #################################################################
+
+
+if (args.mode == 'training'):
+	matrixDimension = (args.M1,args.M2)
+	train(matrixDimension, args.input, args.epocs, args.lr, args.s0, args.sr, saveIn=args.save)
+elif (args.mode == 'test'):
+	test(args.load, args.input)
+
 
 filename = '../data/tp2_training_dataset.csv'
 matrixDimension = (10,10)
 
-nn,X,Z = createEvaluateVari2ables(matrixDimension, filename, 0.0001, 5, 20)
+nn,X_train, Z_train, X_test, Z_test = createEvaluateVariables(matrixDimension, filename, 0.0001, 1, 10, tp=0.2)
 
 acum = 0
 # for epocs in [50,50]:
 # for i in range(100):
 for i in range(200):
 	epocs = 1
-	score = trainAndEval(nn, X, Z, epocs, acum)
+	acu_train, acu_test, distances = trainAndEval(nn, X_train, Z_train, X_test, Z_test, epocs, acum)
 	acum += epocs
-	print "Epocs: %d Score %f Acu: %f Distance: %f Error %f " %(acum, score[0][0],score[0][1],score[0][2],score[1])
+	print "Epocs: %d Score %f Acu Train: %f Acu Test: %f Distances %f " %(acum, acu_test + distances,acu_train, acu_test, distances)
 
 print "(%d,%d) Epocs: %d LR: %f, s0: %f sr: %f" %(matrixDimension[0],matrixDimension[1],acum, nn.lr,nn.sigma0,nn.sigmar)
